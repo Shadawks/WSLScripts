cfbab3247c659178f72914089f52ad104424d799
Kynda <47142028+Shadawks@users.noreply.github.com>
2023-06-29 10:58:16 +0200

    Add files via upload

---
+++ install_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+apt update && apt-cache policy docker-ce
+
+sudo apt install docker-ce
+
+sudo systemctl status docker
+
+docker run -it hello-world
